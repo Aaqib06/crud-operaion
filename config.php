957688c973c525d783404c8c85d53fea510cd02c
Aaqib Bashir <144777648+Aaqib06@users.noreply.github.com>
2023-11-01 16:53:30 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="crud";
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+if($conn==true){
+  echo"";
+} 
+else{
+    echo"failed";
+}
+    ?>
